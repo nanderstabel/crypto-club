881b07b18a31a24a8322f36809bcdfcc6224aa61
nanderstabel <nanderstabel@gmail.com>
2021-11-18 16:35:28 +0100

    Add node peering configurations

--- node_peering.sh
+++ node_peering.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo $PATH
+PEER_PUBKEY=$2
+PEER_NET_ID=$3
+PEER_IP=${PEER_NET_ID%:*}
+
+# Add peer node to wasp-cli.json file
+./wasp-cli set wasp.$1.api $PEER_IP:9090
+./wasp-cli set wasp.$1.nanomsg $PEER_IP:5550
+./wasp-cli set wasp.$1.peering $PEER_IP:4000
+
+# Add peer IP address to whitelist
+jq '.webapi.adminWhitelist += ["'$PEER_ID'"]' config.json|sponge config.json
+
+# Trust peer node
+./wasp-cli peering trust $2 $3
